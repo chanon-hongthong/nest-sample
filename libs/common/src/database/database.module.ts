5ee5dfa6c88042aa37860477a62b38cdb62c0ec2
chanon-hongthong <chanon.hongthong@gmail.com>
2023-11-07 16:23:39 +0700

    import TypeOrmModule in database.module & run command sudo chown -R <user-name> <directory-name>

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,4 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({})
+// https://docs.nestjs.com/techniques/database
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [],
+      synchronize: false,
+    }),
+  ],
+})
 export class DatabaseModule {}
