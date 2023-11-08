7a9832033a345eaa84456663c24edeccffa94a88
chanon-hongthong <chanon.hongthong@gmail.com>
2023-11-08 11:43:19 +0700

    nest generate service /service/user --no-spec & update src > service > user.service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from '@app/common';
 import { UserModule } from './controller/user/user.module';
+import { UserService } from './service/user/user.service';
 
 @Module({
   imports: [DatabaseModule, UserModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UserService],
 })
 export class AppModule {}
