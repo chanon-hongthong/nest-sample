15ea8c584e9c6e7063ff82fe0ad6bfcef1afde28
chanon-hongthong <chanon.hongthong@gmail.com>
2023-11-10 10:03:17 +0700

    nest g controller /controller/user --no-spec

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { DatabaseModule } from '@app/common';
 import { UserModule } from './user.module';
 import { UserService } from './service/user.service';
+import { UserController } from './controller/user/user.controller';
 
 @Module({
   imports: [DatabaseModule, UserModule],
-  controllers: [AppController],
+  controllers: [AppController, UserController],
   providers: [AppService, UserService],
 })
 export class AppModule {}
